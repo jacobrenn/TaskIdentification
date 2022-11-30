876fc9f6365e0f7e73e403f50d7524d2184089fd
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-11-30 07:37:53 -0500

    updating architecture and training procedure
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- cifar10_and_cifar100.py
+++ cifar10_and_cifar100.py
@@ -11,22 +11,24 @@ tf.keras.utils.set_random_seed(3892)
 (cifar10_x_train, cifar10_y_train), (cifar10_x_test, cifar10_y_test) = tf.keras.datasets.cifar10.load_data()
 (cifar100_x_train, cifar100_y_train), (cifar100_x_test, cifar100_y_test) = tf.keras.datasets.cifar100.load_data()
 
-train_x = np.concatenate((cifar10_x_train, cifar100_x_train), axis = 0)/255
+cifar10_x_train = cifar10_x_train/255
+cifar10_x_test = cifar10_x_test/255
+cifar100_x_train = cifar100_x_train/255
+cifar100_x_test = cifar100_x_test/255
+
+train_x = np.concatenate((cifar10_x_train, cifar100_x_train), axis = 0)
 train_y = np.concatenate((cifar10_y_train, cifar100_y_train), axis = 0)
-test_x = np.concatenate((cifar10_x_test, cifar100_x_test), axis = 0)/255
+test_x = np.concatenate((cifar10_x_test, cifar100_x_test), axis = 0)
 test_y = np.concatenate((cifar10_y_test, cifar100_y_test), axis = 0)
-cifar10_train_labels = np.asarray([1] * cifar10_x_train.shape[0] + [0] * cifar100_x_train.shape[0])
-cifar100_train_labels = np.asarray([0] * cifar10_x_train.shape[0] + [1] * cifar100_x_train.shape[0])
-cifar10_test_labels = np.asarray([1] * cifar10_x_test.shape[0] + [0] * cifar100_x_test.shape[0])
-cifar100_test_labels = np.asarray([0] * cifar10_x_test.shape[0] + [1] * cifar100_x_test.shape[0])
+train_task_labels = np.asarray([0] * cifar10_x_train.shape[0] + [1] * cifar100_x_train.shape[0])
+test_class_labels = np.asarray([0] * cifar10_x_test.shape[0] + [1] * cifar100_x_test.shape[0])
 
 indices = np.arange(train_x.shape[0])
 np.random.shuffle(indices)
 
 train_x = train_x[indices]
 train_y = train_y[indices]
-cifar10_train_labels = cifar10_train_labels[indices]
-cifar100_train_labels = cifar100_train_labels[indices]
+cifar10_train_labels = train_task_labels[indices]
 
 input_layer = tf.keras.layers.Input(train_x.shape[1:])
 x = tf.keras.applications.ResNet50(include_top = False)(input_layer)
@@ -46,42 +48,56 @@ x1 = tf.keras.layers.Dropout(0.5)(x1)
 x2 = tf.keras.layers.Dropout(0.5)(x2)
 x1 = tf.keras.layers.BatchNormalization()(x1)
 x2 = tf.keras.layers.BatchNormalization()(x2)
-logit_output = tflow.layers.MultiMaskedDense(100, activation = 'softmax')([x1, x2])
+cifar10_logit_output = tflow.layers.MaskedDense(10, activation = 'softmax')(x1)
+cifar100_logit_output = tflow.layers.MaskedDense(100, activation = 'softmax')(x2)
 
-sel1 = tflow.layers.SelectorLayer(0)(logit_output)
-sel2 = tflow.layers.SelectorLayer(1)(logit_output)
-discerner = tflow.layers.MultiMaskedDense(10, activation = 'relu')([sel1, sel2])
-discerner = tflow.layers.MultiMaskedDense(1, activation = 'sigmoid')(discerner)
-discerner = tflow.layers.MultitaskNormalization()(discerner)
-dsel1 = tflow.layers.SelectorLayer(0)(discerner)
-dsel2 = tflow.layers.SelectorLayer(1)(discerner)
+concatenated_logits = tf.keras.layers.Concatenate()([cifar10_logit_output, cifar100_logit_output])
+discerner = tflow.layers.MaskedDense(100, activation = 'relu')(concatenated_logits)
+discerner = tflow.layers.MaskedDense(1, activation = 'sigmoid')(discerner)
 
-discerner_x1 = tf.keras.layers.Multiply()([sel1, dsel1])
-discerner_x2 = tf.keras.layers.Multiply()([sel2, dsel2])
-
-output = tf.keras.layers.Add()([discerner_x1, discerner_x2])
-
-test_model = tf.keras.models.Model(input_layer, [output, dsel1, dsel2])
-test_model.compile(loss = ['sparse_categorical_crossentropy', 'binary_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adam')
+test_model = tf.keras.models.Model(input_layer, [cifar10_logit_output, cifar100_logit_output, discerner])
+test_model.compile(loss = ['sparse_categorical_crossentropy', 'sparse_categorical_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adam')
 
 test_model = tflow.utils.mask_model(
     test_model,
     50,
     x = train_x[:100],
-    y = [train_y[:100].reshape(-1, 1), cifar10_train_labels[:100].reshape(-1, 1), cifar100_train_labels[:100].reshape(-1, 1)]
+    y = [train_y[:1000].reshape(-1, 1), train_y[:1000].reshape(-1, 1), cifar10_train_labels[:1000].reshape(-1, 1)]
 )
-test_model.compile(loss = ['sparse_categorical_crossentropy', 'binary_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adam')
+test_model.compile(loss = ['sparse_categorical_crossentropy', 'binary_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adam', loss_weights = [1, 0, 0])
 
 tf.keras.utils.plot_model(test_model, to_file = 'cifar10_cifar100_model.png', show_shapes = False)
 
 test_model.fit(
-    train_x,
-    [train_y, cifar10_train_labels, cifar100_train_labels],
+    cifar10_x_train,
+    [cifar10_y_train, cifar100_y_train, cifar10_train_labels[:cifar10_x_train.shape[0]]],
     epochs = 100,
     batch_size = 128,
     verbose = 1,
     validation_split = 0.2,
-    callbacks = tf.keras.callbacks.EarlyStopping(min_delta = 0.004, patience = 3)
+    callbacks = tf.keras.callbacks.EarlyStopping(min_delta = 0.004, patience = 3, restore_best_weights = True)
+)
+
+test_model.compile(loss = ['sparse_categorical_crossentropy', 'binary_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adam', loss_weights = [0, 1, 0])
+test_model.fit(
+    cifar100_x_train,
+    [cifar10_y_train, cifar100_y_train, cifar10_train_labels[:cifar100_x_train.shape[0]]],
+    epochs = 100,
+    batch_size = 128,
+    verbose = 1,
+    validation_split = 0.2,
+    callbacks = tf.keras.callbacks.EarlyStopping(min_delta = 0.004, patience = 3, restore_best_weights = True)
+)
+
+test_model.compile(loss = ['sparse_categorical_crossentropy', 'binary_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adam', loss_weights = [0, 0, 1])
+test_model.fit(
+    train_x,
+    [train_y, train_y, cifar10_train_labels],
+    epochs = 100,
+    batch_size = 128,
+    verbose = 1,
+    validation_split = 0.2,
+    callbacks = tf.keras.callbacks.EarlyStopping(min_delta = 0.004, patience = 3, restore_best_weights = True)
 )
 
 preds = test_model.predict(test_x)
