da23e6a4dd7877df3c5f586efe9b15c6773a56f5
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-11-30 08:09:43 -0500

    changing optimizer
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- cifar10_and_cifar100.py
+++ cifar10_and_cifar100.py
@@ -56,7 +56,7 @@ discerner = tflow.layers.MaskedDense(100, activation = 'relu')(concatenated_logi
 discerner = tflow.layers.MaskedDense(1, activation = 'sigmoid')(discerner)
 
 test_model = tf.keras.models.Model(input_layer, [cifar10_logit_output, cifar100_logit_output, discerner])
-test_model.compile(loss = ['sparse_categorical_crossentropy', 'sparse_categorical_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adam')
+test_model.compile(loss = ['sparse_categorical_crossentropy', 'sparse_categorical_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adamax')
 
 test_model = tflow.utils.mask_model(
     test_model,
@@ -64,7 +64,7 @@ test_model = tflow.utils.mask_model(
     x = cifar10_x_train[:1000],
     y = [cifar10_y_train[:1000], cifar10_y_train[:1000], train_task_labels[:1000].reshape(-1, 1)]
 )
-test_model.compile(loss = ['sparse_categorical_crossentropy', 'sparse_categorical_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adam', loss_weights = [1, 0, 0])
+test_model.compile(loss = ['sparse_categorical_crossentropy', 'sparse_categorical_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adamax', loss_weights = [1, 0, 0])
 
 tf.keras.utils.plot_model(test_model, to_file = 'cifar10_cifar100_model.png', show_shapes = False)
 
@@ -78,7 +78,7 @@ test_model.fit(
     callbacks = tf.keras.callbacks.EarlyStopping(min_delta = 0.004, patience = 3, restore_best_weights = True)
 )
 
-test_model.compile(loss = ['sparse_categorical_crossentropy', 'sparse_categorical_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adam', loss_weights = [0, 1, 0])
+test_model.compile(loss = ['sparse_categorical_crossentropy', 'sparse_categorical_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adamax', loss_weights = [0, 1, 0])
 test_model.fit(
     cifar100_x_train,
     [cifar10_y_train, cifar100_y_train, train_task_labels[:cifar100_x_train.shape[0]]],
@@ -89,7 +89,7 @@ test_model.fit(
     callbacks = tf.keras.callbacks.EarlyStopping(min_delta = 0.004, patience = 3, restore_best_weights = True)
 )
 
-test_model.compile(loss = ['sparse_categorical_crossentropy', 'sparse_categorical_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adam', loss_weights = [0, 0, 1])
+test_model.compile(loss = ['sparse_categorical_crossentropy', 'sparse_categorical_crossentropy', 'binary_crossentropy'], metrics = ['accuracy'], optimizer = 'adamax', loss_weights = [0, 0, 1])
 test_model.fit(
     train_x,
     [train_y, train_y, train_task_labels],
